54ac006b6bc47f219ef95d7f9cfcd846b673ddb7
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2023-07-28 22:13:16 +0530

    MyAppointment component

---
+++ src/components/Dashboard/Patient/MyAppointments/AppointmentCard.js
@@ -0,0 +1,60 @@
+import React from "react";
+import styles from "./AppointmentCard.module.css";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+} from "@mui/material";
+import { Cancel, Contactless, Feedback } from "@mui/icons-material";
+
+export default function AppointmentCard(props) {
+  return (
+    <Card sx={{ maxWidth: "100%", textAlign: "center" }} variant="outlined">
+      <CardContent>
+        <br />
+        <Typography
+          sx={{ fontSize: "0.9rem" }}
+          color="text.secondary"
+          gutterBottom
+        >
+          Appointment with
+        </Typography>
+        <Typography variant="h5" component="div">
+          {props.appointment.doctor}
+        </Typography>
+        <Typography sx={{ mb: 2.5, fontSize: "0.8rem" }} color="text.secondary">
+          {`(${props.appointment.speciality})`}
+        </Typography>
+        <Typography
+          sx={{ mb: 1.5, fontSize: "1rem" }}
+          color="text.secondary"
+        >{`${props.appointment.date} (${props.appointment.time})`}</Typography>
+        <br />
+        <CardActions sx={{ justifyContent: "space-between" }} disableSpacing>
+          <Button
+            variant="contained"
+            color="error"
+            size="small"
+            endIcon={<Cancel />}
+            // onClick={handleCancel}
+            // disabled={dateInPast() ? true : false}
+          >
+            Cancel
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            size="small"
+            endIcon={<Contactless />}
+            // onClick={handleCancel}
+            // disabled={dateInPast() ? true : false}
+          >
+            Payment
+          </Button>
+        </CardActions>
+      </CardContent>
+    </Card>
+  );
+}
---
+++ src/components/Dashboard/Patient/MyAppointments/MyAppointments.js
@@ -0,0 +1,60 @@
+import React from "react";
+import styles from "./MyAppointments.module.css";
+import AppointmentCard from "./AppointmentCard";
+import { Grid } from "@mui/material";
+import Navbar from "../../../Navbar/Navbar";
+
+const appointments = [
+  {
+    doctor: "Dr. A. K. Arya",
+    speciality: "MBBS, Surgeon",
+    date: "30 Jan 2023",
+    time: "6pM - 7pM",
+    payment: false,
+  },
+  {
+    doctor: "Dr. S. K. Choudhary",
+    speciality: "MBBS, Surgeon",
+    date: "30 Jan 2023",
+    time: "6pM - 7pM",
+    payment: false,
+  },
+  {
+    doctor: "Dr. B. K. Pandit",
+    speciality: "MBBS, Surgeon",
+    date: "30 Jan 2023",
+    time: "6pM - 7pM",
+    payment: false,
+  },
+  {
+    doctor: "Dr. Mukharjee",
+    speciality: "MBBS, Surgeon",
+    date: "30 Jan 2023",
+    time: "6pM - 7pM",
+    payment: false,
+  },
+  {
+    doctor: "Dr. Dwarka Prasad",
+    speciality: "MBBS, Surgeon",
+    date: "30 Jan 2023",
+    time: "6pM - 7pM",
+    payment: false,
+  },
+];
+
+export default function MyAppointments() {
+  return (
+    <div className={styles.container}>
+      <Navbar />
+      <div className={styles.cardContainer}>
+        <Grid container spacing={3}>
+          {appointments.map((appointment, index) => (
+            <Grid key={index} item xs={12}>
+              <AppointmentCard appointment={appointment} />
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+    </div>
+  );
+}
