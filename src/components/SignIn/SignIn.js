2f116a5f521638a5cb3815f9fb9b56c4b6a24c17
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2024-02-13 21:23:41 +0530

    modified auth ui

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -45,63 +45,68 @@ export default function SignIn() {
 
   return (
     <div className={styles.container}>
-      <Box
-        component="form"
-        className={styles.form}
-        validate="true"
-        onSubmit={handleSubmit}
-      >
-        <Avatar
-          alt="auth logo"
-          src="/authimg.png"
-          sx={{ width: 100, height: 100 }}
-        />
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-
-        {/* Email Field */}
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          id="email"
-          type="email"
-          label="Email Address"
-          name="email"
-          value={email}
-          autoComplete="email"
-          autoFocus
-          onChange={(event) => setEmail(event.target.value)}
-        />
-
-        {/* Password Field */}
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          name="password"
-          label="Password"
-          type="password"
-          id="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
-
-        {/* Submit Button */}
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          sx={{ mt: 3, mb: 2 }}
+      <div className={styles.col1}>
+        <Box
+          component="form"
+          className={styles.form}
+          validate="true"
+          onSubmit={handleSubmit}
         >
-          Sign In
-        </Button>
+          <Avatar
+            alt="auth logo"
+            src="/authimg.png"
+            sx={{ width: 100, height: 100 }}
+          />
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
 
-        <Link to="/signup" className={styles.linkBtn}>
-          {"Don't have an account? Sign Up"}
-        </Link>
-      </Box>
+          {/* Email Field */}
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            type="email"
+            label="Email Address"
+            name="email"
+            value={email}
+            autoComplete="email"
+            autoFocus
+            onChange={(event) => setEmail(event.target.value)}
+          />
+
+          {/* Password Field */}
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+
+          {/* Submit Button */}
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+
+          <Link to="/signup" className={styles.linkBtn}>
+            {"Don't have an account? Sign Up"}
+          </Link>
+        </Box>
+      </div>
+      <div className={styles.col2}>
+        <img src="/medicine.svg" alt="doctor" draggable="false" />
+      </div>
     </div>
   );
 }
